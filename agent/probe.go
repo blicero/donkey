4ac0ceaaded9f30661a4f66be10df6a36b1537cd
Benjamin Walkenhorst <krylon@posteo.de>
2024-06-14 21:38:55 +0200

    Remove file probe.go from package agent. I might add it again later.

--- agent/probe.go
+++
@@ -1,7 +0,0 @@
-// /home/krylon/go/src/github.com/blicero/donkey/agent/probe.go
-// -*- mode: go; coding: utf-8; -*-
-// Created on 13. 06. 2024 by Benjamin Walkenhorst
-// (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-06-13 19:09:05 krylon>
-
-package agent
