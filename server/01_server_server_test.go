03d712cc306c5bcf5fad5deab537a408dcbdbf8a
Benjamin Walkenhorst <krylon@posteo.de>
2024-06-20 13:35:08 +0200

    First, puny test case for Server

--- server/01_server_server_test.go
+++ server/01_server_server_test.go
@@ -2,8 +2,21 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 19. 06. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-06-19 18:52:26 krylon>
+// Time-stamp: <2024-06-20 13:29:14 krylon>
 
 package server
 
+import "testing"
+
+const testAddr = "[::1]:4197"
+
 var srv *Server
+
+func TestCreateServer(t *testing.T) {
+	var err error
+
+	if srv, err = Create(testAddr); err != nil {
+		srv = nil
+		t.Fatalf("Failed to create Server: %s", err.Error())
+	}
+} // func TestCreateServer(t *testing.T)
